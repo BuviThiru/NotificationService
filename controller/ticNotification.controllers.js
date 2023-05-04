fc5a46ab5d840dc79d565bf44d33a6149c92a678
Buvaneshwari <thirubuvana@gmail.com>
2023-05-04 11:15:42 +0530

    connected to crm

--- controller/ticNotification.controllers.js
+++ controller/ticNotification.controllers.js
@@ -2,6 +2,7 @@ const {createTicNotificationSer} = require('../services/ticNotification.services
 
 exports.createTicNotification = async (req, res) => {
   try {
+    console.log("request received in notification server",req)
      const notification = await createTicNotificationSer(req.body);
     if (notification.result) {
       return res.status(200).send({
