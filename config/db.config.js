0cd9197df23a31356789254ca27027d025eecf07
Buvaneshwari <thirubuvana@gmail.com>
2023-05-04 10:39:41 +0530

    db deployed

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,3 @@
 require("dotenv").config()
-const notification_URI = process.env.Mongo_URL ;
-
-
+const notification_URI = process.env.Mongo_URL || 'mongodb://127.0.0.1:27017/notificationDB';
 module.exports = notification_URI
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = require('./config/server.config')
 const URL = require("./config/db.config")
 const mongoose = require("mongoose")
 const ticketNotificationRoutes = require("./routes/ticNotication.routes")
-const {job} = require('./crons/ticNotification.cron')
+// const {job} = require('./crons/ticNotification.cron')
 
 const {sendNotificationMail} = require('./notifier/emailNotifier')
 const bodyParcer = require('body-parser');
