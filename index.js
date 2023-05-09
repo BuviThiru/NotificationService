9b5b1cf914663c661c9e59adbb3debdc28d36333
Buvaneshwari <thirubuvana@gmail.com>
2023-05-09 10:36:00 +0530

    chors added

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const bodyParcer = require('body-parser');
 
 app.use(bodyParcer.json());
 app.use(bodyParcer.urlencoded({extended:true}))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 ticketNotificationRoutes(app);
 
 app.listen(PORT,()=>{
